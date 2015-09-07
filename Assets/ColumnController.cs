064fc7d9c9745cd2a16f752cefcbc06c8a79d894
Danubian <stjohnzack@yahoo.com>
2015-09-07 19:22:32 -0400

    Adding ColumnController to handle object movement and positioning on screen

---
+++ Assets/ColumnController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColumnController : MonoBehaviour {
+	//	public float horizontalSpeed;
+	public RectTransform boundaries;
+	//	public float leftBound;
+	//	public float rightBound;
+	public float basePos;
+	//	private float width = 1f;
+	public int max = 8; 
+	public float chunkSize;
+	public float length;
+
+	// Use this for initialization
+	void Start () {
+		Vector3[] corners = new Vector3[4];
+		boundaries.GetWorldCorners(corners);
+		float leftBound = corners[0].x;
+		float rightBound = corners[2].x;
+		length = rightBound - leftBound;
+		chunkSize = length/max;
+		
+		basePos = leftBound + chunkSize/2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public float GetIndexPos(int index)
+	{
+		return (chunkSize * index) + basePos;
+	}
+
+	public bool InBounds(int index)
+	{
+		bool leftBounds = index >= 0;	
+		bool rightBounds = index < max;
+		return  leftBounds && rightBounds;
+	}
+}
--- Assets/Scripts/SwagObject.cs
+++ Assets/Scripts/SwagObject.cs
@@ -2,7 +2,10 @@
 using System.Collections;
 
 public class SwagObject : MonoBehaviour {
-
+	
+	public ColumnController colControl;
+	
+	public int index;
 	// Use this for initialization
 	void Start () {
 	
@@ -11,5 +14,32 @@ public class SwagObject : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void Move(int newIndex)
+	{
+		if(colControl.InBounds(newIndex))
+		{
+			float newPosX = colControl.GetIndexPos(newIndex);
+			Vector3 newPos = new Vector3(newPosX, transform.position.y, transform.position.z);
+			this.transform.position = newPos;
+			index = newIndex;
+		}
+	}
+
+	private bool moveLock = false;
+	private IEnumerator MoveToPosition(Vector3 startPos, Vector3 endPos, float time)
+	{
+		moveLock = true;
+		float elapsedTime = 0;
+		this.transform.position = startPos;
+		
+		while (elapsedTime < time)
+		{
+			this.transform.position = Vector3.Lerp(this.transform.position, endPos, (elapsedTime / time));
+			elapsedTime += Time.deltaTime;
+			yield return null;
+		}
+		moveLock = false;
 	}
 }
