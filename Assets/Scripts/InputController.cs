192bbe451b64499e381b2f367d5dfbd23fef1ed0
Danubian <stjohnzack@yahoo.com>
2015-09-07 17:40:57 -0400

    Adding Game Scene. Removing Temp

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputController : MonoBehaviour {
+	public Player player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnLeftClick()
+	{
+		Debug.Log("Left Side Clicked");
+		if(player != null)
+		{
+			player.MoveLeft();
+		}
+	}
+
+	public void OnRightClick()
+	{
+		Debug.Log("Right Side Clicked");
+		if(player != null)
+		{
+			player.MoveRight();
+		}
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : SwagObject {
+	public float horizontalSpeed;
+	public RectTransform bounds;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void MoveLeft()
+	{
+		if(this.transform.position.x < bounds.rect.xMin)
+		{
+			Move(-1 * horizontalSpeed);
+		}
+	}
+
+	public void MoveRight()
+	{
+		Move(horizontalSpeed);
+	}
+
+	private void Move(float speed)
+	{
+		Vector3 pos = this.transform.position;
+		float newPosX = pos.x + speed;
+		this.transform.position = new Vector3(newPosX, pos.y, pos.z);
+	}
+}
